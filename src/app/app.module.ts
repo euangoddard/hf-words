d60343653e46732acc13ebead01645d36d71764d
Euan Goddard <euan@stockopedia.com>
2020-01-27 20:50:38 +0000

    Created PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { NgxAliasModule } from 'ngx-alias';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { LetterComponent } from './letter/letter.component';
 
 @NgModule({
   declarations: [AppComponent, LetterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, NgxAliasModule],
+  imports: [BrowserModule, BrowserAnimationsModule, NgxAliasModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
